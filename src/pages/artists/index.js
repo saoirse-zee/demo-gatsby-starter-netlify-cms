5bcbe538b9bbeee282a34c46ef0c8eb39006ea10
Emma Smith <saoirse-zee@users.noreply.github.com>
2020-02-03 19:22:35 -0500

    Add Layout to Artist list

--- src/pages/artists/index.js
+++ src/pages/artists/index.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+import Layout from "../../components/Layout";
+
 export default ({ data }) => {
   return (
-    <div>
-      <h2>Number of artists: {data.allMarkdownRemark.totalCount}</h2>
+    <Layout>
       <div>
-        {data.allMarkdownRemark.nodes.map((node, index) => (
-          <p key={index}>
-            <span>{node.frontmatter.title}</span>
-            <span> </span>
-            <span>{node.frontmatter.spontaneity * 100}% spontaneous</span>
-          </p>
-        ))}
+        <h2>Number of artists: {data.allMarkdownRemark.totalCount}</h2>
+        <div>
+          {data.allMarkdownRemark.nodes.map((node, index) => (
+            <p key={index}>
+              <span>{node.frontmatter.title}</span>
+              <span> </span>
+              <span>{node.frontmatter.spontaneity * 100}% spontaneous</span>
+            </p>
+          ))}
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
